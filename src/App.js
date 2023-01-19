f99978b8bee02b0d18a0e93be9c3305f287b3f42
Shahadat Hossain <shahadat3669@gmail.com>
2023-01-19 21:41:26 +0600

    Add scss package and setup it.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-const App = () => {
-  return <div className="App">Hi from book store</div>;
-};
+import React from 'react';
+
+import styles from './App.module.scss';
+
+const App = () => (
+  <div className={styles.main}>
+    <h1 className={styles.main__heading}>todo</h1>
+  </div>
+);
 
 export default App;
