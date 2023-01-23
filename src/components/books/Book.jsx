966d3103a74e2c504cbd306822dc116d5f74d85d
Shahadat Hossain <shahadat3669@gmail.com>
2023-01-24 03:49:19 +0600

    Dispatch deleteBook in book.jsx file.

--- src/components/books/Book.jsx
+++ src/components/books/Book.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 
 import styles from './Book.module.scss';
+import { deleteBook } from '../../redux/books/books';
 
 const Book = ({ book }) => {
-  const {
-    author, title, category, progress,
-  } = book;
-  const { currentChapter } = progress;
+  // eslint-disable-next-line
+  const { item_id, author, title, category, completed } = book;
+  const dispatch = useDispatch();
+  const handleRemoveBtnClick = () => {
+    dispatch(deleteBook(item_id));
+  };
   return (
     <div className={styles.book}>
       <div className={styles.book__left}>
@@ -17,13 +21,23 @@ const Book = ({ book }) => {
           <p className={styles.book__author}>{author}</p>
         </div>
         <div className={styles['book__btn-group']}>
-          <button type="button" className={styles.book__btn}>
+          <button
+            type="button"
+            className={styles.book__btn}
+          >
             Comments
           </button>
-          <button type="button" className={styles.book__btn}>
+          <button
+            type="button"
+            className={styles.book__btn}
+            onClick={handleRemoveBtnClick}
+          >
             Remove
           </button>
-          <button type="button" className={styles.book__btn}>
+          <button
+            type="button"
+            className={styles.book__btn}
+          >
             Edit
           </button>
         </div>
@@ -31,25 +45,28 @@ const Book = ({ book }) => {
       <div className={styles.book__right}>
         <div className={styles['book__progress-container']}>
           <div className={styles['book__progress-container']}>
-            <h4 className={styles['book__progress-title']}>64%</h4>
+            <h4 className={styles['book__progress-title']}>
+              {`${completed}%`}
+            </h4>
             <p className={styles['book__progress-complete']}>Completed</p>
           </div>
         </div>
-        <div className={styles['book__chapter-details']}>
+        {/* <div className={styles['book__chapter-details']}>
           <div className={styles['book__chapter-container']}>
             <span className={styles['book__chapter-name']}>
               Current Chapter
             </span>
-            <h4 className={styles['book__chapter-title']}>
-              {currentChapter}
-            </h4>
+            <h4 className={styles['book__chapter-title']}>{completed}</h4>
           </div>
           <div className={styles['book__chapter-btn-wrapper']}>
-            <button type="button" className={styles['book__chapter-btn']}>
+            <button
+              type="button"
+              className={styles['book__chapter-btn']}
+            >
               Update Progress
             </button>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
