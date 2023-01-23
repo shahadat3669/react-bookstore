92cae1fc5097440a51f260e81c576661b6249260
Shahadat Hossain <shahadat3669@gmail.com>
2023-01-24 01:34:43 +0600

    Import categories reducer and add in redux store.

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { combineReducers, legacy_createStore as createStore } from 'redux';
 
 import booksReducer from './books/books';
+import categoriesReducer from './categories/categories';
 
 const reducers = combineReducers({
   book: booksReducer,
+  categories: categoriesReducer,
 });
 
 const store = createStore(reducers);
