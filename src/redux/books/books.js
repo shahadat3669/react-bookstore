d55b6ae92a9da077508c009a6c6d144ab9810a07
Shahadat Hossain <shahadat3669@gmail.com>
2023-01-24 01:31:39 +0600

    Add book reducer.

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -11,3 +11,25 @@ export const deleteBook = (payload) => ({
   type: DELETE_BOOK,
   payload,
 });
+
+const reducer = (state = defaultSate, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [
+        ...state,
+        {
+          item_id: action.payload.item_id,
+          title: action.payload.title,
+          author: action.payload.author,
+          completed: 0,
+          category: action.payload.category,
+        },
+      ];
+    case DELETE_BOOK:
+      return state.filter((book) => book.item_id !== action.payload);
+    default:
+      return state;
+  }
+};
+
+export default reducer;
