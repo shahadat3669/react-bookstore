80579ad7ccd7950b6bd85d10ad2cf28fd3d88c63
Shahadat Hossain <shahadat3669@gmail.com>
2023-01-24 03:50:50 +0600

    Update bookList.jsx file for update ui with books.

--- src/components/books/BookList.jsx
+++ src/components/books/BookList.jsx
@@ -1,52 +1,29 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import styles from './BookList.module.scss';
-
 import Book from './Book';
 import AddBook from './AddBook';
 
-const tempData = [
-  {
-    author: 'Suzanne Collins',
-    category: 'Action',
-    item_id: 'mock-1',
-    progress: { completed: '64', currentChapter: 'Chapter 17' },
-    completed: '64',
-    currentChapter: 'Chapter 17',
-    title: 'The Hunger Games',
-  },
-  {
-    author: 'Suzanne Collins',
-    category: 'Action',
-    item_id: 'mock-2',
-    progress: { completed: '64', currentChapter: 'Chapter 17' },
-    completed: '64',
-    currentChapter: 'Chapter 17',
-    title: 'The Hunger Games',
-  },
-  {
-    author: 'Suzanne Collins',
-    category: 'Action',
-    item_id: 'mock-3',
-    progress: { completed: '64', currentChapter: 'Chapter 17' },
-    completed: '64',
-    currentChapter: 'Chapter 17',
-    title: 'The Hunger Games',
-  },
-];
+const BookList = () => {
+  const books = useSelector((state) => state.book);
 
-const BookList = () => (
-  <section className={styles.books}>
-    <div className={styles.books__list}>
-      {tempData.map((book) => (
-        <Book key={book.item_id} book={book} />
-      ))}
-    </div>
+  return (
+    <section className={styles.books}>
+      <div className={styles.books__list}>
+        {books.map((book) => (
+          <Book
+            key={book.item_id}
+            book={book}
+          />
+        ))}
+      </div>
 
-    <div className={styles.books__new}>
-      <AddBook />
-    </div>
-  </section>
-);
+      <div className={styles.books__new}>
+        <AddBook />
+      </div>
+    </section>
+  );
+};
 
 export default BookList;
