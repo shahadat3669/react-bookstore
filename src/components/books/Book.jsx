4b97e21ea34450d421ad20abc9e89fe6df65953f
Shahadat Hossain <120262758+shahadat-pciu@users.noreply.github.com>
2023-01-28 18:08:10 +0600

    Update Book jsx ui colors

--- src/components/books/Book.jsx
+++ src/components/books/Book.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 import styles from './Book.module.scss';
 import { deleteBook } from '../../redux/books/books';
 
 const Book = ({ book }) => {
-  const {
-    item_id: itemID, author, title, category, completed,
-  } = book;
+  // eslint-disable-next-line
+  const { item_id: itemID, author, title, category, completed } = book;
   const dispatch = useDispatch();
   const handleRemoveBtnClick = () => {
     dispatch(deleteBook(itemID));
   };
+
+  const percentage = 66;
+
   return (
     <div className={styles.book}>
       <div className={styles.book__left}>
@@ -44,12 +48,35 @@ const Book = ({ book }) => {
         </div>
       </div>
       <div className={styles.book__right}>
-        <div className={styles['book__progress-container']}>
-          <div className={styles['book__progress-container']}>
-            <h4 className={styles['book__progress-title']}>
-              {`${completed || 0}%`}
-            </h4>
-            <p className={styles['book__progress-complete']}>Completed</p>
+        <div className={styles['book__progress-wrapper']}>
+          <CircularProgressbar
+            value={percentage}
+            styles={buildStyles({
+              pathColor: '#0290ff',
+            })}
+          />
+        </div>
+        <div className={styles['book__progress-text-wrapper']}>
+          <h4 className={styles['book__progress-title']}>
+            {`${completed || 0}%`}
+          </h4>
+          <p className={styles['book__progress-complete']}>Completed</p>
+        </div>
+        <div className={styles['book__progress-divider']} />
+        <div className={styles['book__chapter-details']}>
+          <div className={styles['book__chapter-container']}>
+            <span className={styles['book__chapter-name']}>
+              Current Chapter
+            </span>
+            <h4 className={styles['book__chapter-title']}>Chapter 1</h4>
+          </div>
+          <div className={styles['book__chapter-btn-wrapper']}>
+            <button
+              type="button"
+              className={styles['book__chapter-btn']}
+            >
+              Update Progress
+            </button>
           </div>
         </div>
       </div>
