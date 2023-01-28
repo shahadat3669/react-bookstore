294cd4730b33d93cdcc4bb1af15603183cd0b6b8
Shahadat Hossain <120262758+shahadat-pciu@users.noreply.github.com>
2023-01-28 18:13:32 +0600

    Fix linter errors.

--- src/components/books/Book.jsx
+++ src/components/books/Book.jsx
@@ -8,7 +8,6 @@ import styles from './Book.module.scss';
 import { deleteBook } from '../../redux/books/books';
 
 const Book = ({ book }) => {
-  // eslint-disable-next-line
   const { item_id: itemID, author, title, category, completed } = book;
   const dispatch = useDispatch();
   const handleRemoveBtnClick = () => {
